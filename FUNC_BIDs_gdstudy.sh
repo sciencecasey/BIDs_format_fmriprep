785c63eedda492615f19c1261f6294d6d1566ac5
Casey Jayne <sciencecaseyj@gmail.com>
2019-09-05 15:47:01 -0700

    Add files via upload
    errors in the validator, added select fields to the .jsons and removed TRs from name, instead validating it in .json RepetitionTime

--- FUNC_BIDs_gdstudy.sh
+++ FUNC_BIDs_gdstudy.sh
@@ -59,6 +59,50 @@ for subj in $subjects; do  #change this to the folder you want it to be
 	#Changing directory into the subject folder
 	cd ${outputdir}/sub-${subj}/ses-${session}
 
+	#add Tr to .json file
+	for funcjson in $(ls *.json); do
+		reptimeexist=$(cat ${funcjson} | jq '.RepetitionTime')
+		if [[ ${reptimeexist} == 'null' ]]; then
+			jsonname="${funcjson%.*}"
+			if [[ ${jsonname} = *REST* ]]; then
+				temtr=$(echo $tempr | awk -F '[_]' '{print $6}')
+				Tr=$(echo $temptr | awk -F '[R]' '{print $2}')
+			else
+				temptr=$(echo $jsonname | awk -F '[_]' '{print $5}')
+				Tr=$(echo $temptr | awk -F '[R]' '{print $2}')
+			fi
+			jq '. |= . + {"RepetitionTime":"'${Tr}'"}' ${funcjson} > TRsadd.json
+		#jq is a json filter >jq [options...] filter [files...]
+		#here, we add the field on the right side of |= . + to the output Defined
+		#by the left side '.  ; apply that to the funcjson and then
+		#name that file TRsadd.json
+			rm ${funcjson} #remove the original
+			mv TRsadd.json ${funcjson} #rename new file with original name
+			echo "TRs RepitionTime was added to ${jsonname}"
+		else
+			Repquotevalue=$(jq '.RepetitionTime' ${funcjson})
+			Repvalue=$(echo $Repquotevalue | cut -d '"' -f2)
+			jsonname="${funcjson%.*}"
+			if [[ ${jsonname} = *REST* ]]; then
+				temtr=$(echo $jsonname | awk -F '[_]' '{print $6}')
+				repfield=$(echo $temtr | awk -F '[R]' '{print $2}')
+				if [ $Repvalue == $repfield ]; then
+					echo "RepTime is present and matches the tasklevel in ${jsonname}"
+				else
+					echo "RepTime and Replabel don't match for ${jsonname}. RepTime is $Repvalue, RepField is $repfield"
+				fi
+			else
+				temptr=$(echo $jsonname | awk -F '[_]' '{print $5}')
+				repfield=$(echo $temptr | awk -F '[R]' '{print $2}')
+				if [ $Repvalue == $repfield ]; then
+					echo "RepTime is present and matches the tasklevel in ${jsonname}"
+				else
+					echo "RepTime and Replabel don't match for ${jsonname}. RepTime is $Repvalue, RepField is $repfield"
+				fi
+			fi
+		fi
+	done
+
 	##Rename func files
 	#Break the func down into each task
 	#BODYMORPH task
@@ -80,7 +124,7 @@ for subj in $subjects; do  #change this to the folder you want it to be
 			tempb=$(ls -1 $b | sed '1q;d')
 			tempbfile="${tempb%.*}"
 			tempbext="${tempb##*.}"
-			Tr=$(echo $tempb | awk -F '[_]' '{print $5}')
+			#Tr=$(echo $tempb | awk -F '[_]' '{print $5}')
 			#num=$(echo $tempbfile | rev | cut -d '_' -f1 | rev) #I didn't know if this num was important but it's not
 			#if [[ $num == *.nii ]]
 			#	then num=${num%.nii}
@@ -89,8 +133,8 @@ for subj in $subjects; do  #change this to the folder you want it to be
 			run=$(echo $tempbfile | awk -F'[_]' '{print $3}')
 		#always backup so that you don't rewrite the filenames
 		#this makes a ~1~ if there are more than one
-			mv -n ${tempb} sub-${subj}_ses-${session}_task-bodymorph_run-${run}_acq-TP${tp}-${Tr}_bold.${tempbext}
-			echo "${tempb} changed to sub-${subj}_ses-${session}_task-bodymorph_run-${run}_acq-TP${tp}-${Tr}_bold.${tempbext}"
+			mv -n ${tempb} sub-${subj}_ses-${session}_task-bodymorph_acq-TP${tp}_run-${run}_bold.${tempbext}
+			echo "${tempb} changed to sub-${subj}_ses-${session}_task-bodymorph_acq-TP${tp}_run-${run}_bold.${tempbext}"
 		done
 	done
 	blfiles=$(ls -1 *BODY_LOCALIZE* | wc -l)
@@ -106,12 +150,12 @@ for subj in $subjects; do  #change this to the folder you want it to be
 			tempb=$(ls -1 $b | sed '1q;d')
 			tempbfile="${tempb%.*}"
 			tempbext="${tempb##*.}"
-			Tr=$(echo $tempb | awk -F '[_]' '{print $5}')
+			#Tr=$(echo $tempb | awk -F '[_]' '{print $5}')
 			taskname=$(echo $tempbfile | awk -F'[_]' '{print $2$3}')
 		#always backup so that you don't rewrite the filenames
 		#this makes a ~1~ if there are more than one
-			mv -n ${tempb} sub-${subj}_ses-${session}_task-localizer_acq-TP${tp}-${Tr}_bold.${tempbext}
-			echo "${tempb} changed to sub-${subj}_ses-${session}_task-localizer_acq-TP${tp}-${Tr}_bold.${tempbext}"
+			mv -n ${tempb} sub-${subj}_ses-${session}_task-localizer_acq-TP${tp}_bold.${tempbext}
+			echo "${tempb} changed to sub-${subj}_ses-${session}_task-localizer_acq-TP${tp}_bold.${tempbext}"
 		done
 	done
 	restfiles=$(ls *REST* | wc -l)
@@ -126,17 +170,73 @@ for subj in $subjects; do  #change this to the folder you want it to be
 				tempr=$(ls -1 $r | sed '1q;d')
 				temprfile="${tempr%.*}"
 				temprext="${tempr##*.}"
-				Tr=$(echo $tempr | awk -F '[_]' '{print $6}')
-				mv -n $tempr sub-${subj}_ses-${session}_task-rest_acq-TP${tp}-${Tr}_bold.${temprext}
-				echo "$tempr changed to sub-${subj}_ses-${session}_task-rest_acq-TP${tp}-${Tr}_bold.${temprext}"
+				#Tr=$(echo $tempr | awk -F '[_]' '{print $6}')
+				mv -n $tempr sub-${subj}_ses-${session}_task-rest_acq-TP${tp}_bold.${temprext}
+				echo "$tempr changed to sub-${subj}_ses-${session}_task-rest_acq-TP${tp}_bold.${temprext}"
 			done
 	done
 
 	#Rename all *.gz to *.nii.gz and move to correct folder
-	for i in sub-${subj}_*.gz; do
-		mv $i "${i%.*}.nii.gz"
+	for i in sub-${subj}_*.gz; do mv $i "${i%.*}.nii.gz"; done
+
+	#check BIDs Valid
+	##.json files need to have Vol or Slice timing, TaskName, and RepTime defined
+	for funcjson in $(ls *.json); do
+		#rep time in json?
+		repeatexist=$(cat ${funcjson} | jq '.RepetitionTime')
+		if [[ ${repeatexist} == "null" ]]; then
+			echo "${funcjson} doesn't have RepitionTime defined"
+		else
+			echo "${funcjson} has RepitionTime defined"
+		fi
+		#Volume or Slice Timing defined?
+		volexist=$(cat ${funcjson} | jq '.VolumeTiming')
+		sliceexist=$(cat ${funcjson} | jq '.SliceTiming')
+		if [[ ${volexist} == "null" && ${sliceexist} == "null" ]]; then
+			echo "${funcjson} doesn't have Volume Timing or Slice Timing defined"
+		else
+			if [[ ${volexist} == "null" ]]; then
+				echo "${funcjson} has Slice Timing defined"
+				#check that slice Timing is less than TR
+				sliceTR=$(cat ${funcjson} | jq '.SliceTiming[] | select(.>="$repeatexist")')
+				if [ -z ${sliceTR} ]; then
+					echo "All Slice Timing is less than TR"
+				else
+					echo "${funcjson} has Slice Timing error"
+				fi
+			else
+				echo "${funcjson} has Volume Timing Defined"
+			fi
+		fi
+		#TaskName exist in json?
+		taskexist=$(cat ${funcjson} | jq '.TaskName')
+		if [[ ${taskexist} == 'null' ]]; then
+			jsonname="${funcjson%.*}"
+			taskfield=$(echo $jsonname | cut -d '_' -f3 | cut -d '-' -f2)
+			jq '. |= . + {"TaskName":"'${taskfield}'"}' ${funcjson} > tasknameadd.json
+			#jq is a json filter >jq [options...] filter [files...]
+			#here, we add the field on the right side of |= . + to the output Defined
+			#by the left side '.  ; apply that to the funcjson and then
+			#name that file tasknameadd.json
+			rm ${funcjson} #remove the original
+			mv tasknameadd.json ${funcjson} #rename new file with original name
+			echo "TaskName was added to ${jsonname} and matches the tasklevel in filename"
+		else
+			Taskquotevalue=$(jq '.TaskName' ${funcjson})
+			Taskvalue=$(echo $Taskquotevalue | cut -d '"' -f2)
+			jsonname="${funcjson%.*}"
+			taskfield=$(echo $jsonname | cut -d '_' -f3 | cut -d '-' -f2)
+			if [ $Taskvalue == $taskfield ]; then
+				echo "TaskName is present and matches the tasklavel in ${jsonname}"
+			else
+				echo "TaskName and tasklabel don't match for ${jsonname}"
+			fi
+		fi
 	done
+
+	#move files to bold folder!
 	for files in *bold*; do mv $files func; done
+
 
 	####Diffusion Organization####
 	#Create subject folder
@@ -145,7 +245,7 @@ for subj in $subjects; do  #change this to the folder you want it to be
 	###Convert DWI dcm to nii
 	cd ${sourcedir}
 	for direcs in {*DWI*,*DTI*}; do
-		dcm2niix -b y -z y -o ${outputdir}/sub-${subj}/ses-${session}/ -f ${subj}_%p_%s ${sourcedir}/*_${subj}_S${session}/dicom/Prisma*/201*****/FEU*/${direcs}
+		dcm2niix -b y -z y -v y -o ${outputdir}/sub-${subj}/ses-${session}/ -f ${subj}_%p_%s ${sourcedir}/*_${subj}_S${session}/dicom/Prisma*/201*****/FEU*/${direcs}
 	done
 
 	#Changing directory into the subject folder
@@ -166,8 +266,8 @@ for subj in $subjects; do  #change this to the folder you want it to be
 			tempdext=${tempd##*.}
 			tempdir=$(echo $tempd | awk -F '_' '{print $3$4}')
 			dir="${tempdir##dir}"
-			mv -n $tempd sub-${subj}_ses-${session}_acq-TP${tp}-auto-7B5_dir-${dir}_dwi.${tempdext}
-			echo "changed $tempd to sub-${subj}_ses-${session}_acq-TP${tp}-auto-7B5_dir-${dir}_dwi.${tempdext}"
+			mv -n $tempd sub-${subj}_ses-${session}_acq-TP${tp}auto7B5_dir-${dir}_dwi.${tempdext}
+			echo "changed $tempd to sub-${subj}_ses-${session}_acq-TP${tp}auto7B5_dir-${dir}_dwi.${tempdext}"
 		done
 	done
 	dtifiles=$(ls -1 *DTI* | wc -l)
@@ -187,20 +287,14 @@ for subj in $subjects; do  #change this to the folder you want it to be
 			tempdir=$(echo $tempd | awk -F '_' '{print $3$4}')
 			dir=$(echo $tempdir | awk -F 'dir' '{print $1$2}')
 			b=$(echo $tempd | awk -F '_' '{print $5$6}')
-			mv -n $tempd sub-${subj}_ses-${session}_acq-TP${tp}-${b}_dir-${dir}_dwi.${tempdext}
-			echo "changed $tempd to sub-${subj}_ses-${session}_acq-TP${tp}-${b}_dir-${dir}_dwi.${tempdext}"
+			mv -n $tempd sub-${subj}_ses-${session}_acq-TP${tp}${b}_dir-${dir}_dwi.${tempdext}
+			echo "changed $tempd to sub-${subj}_ses-${session}_acq-TP${tp}${b}_dir-${dir}_dwi.${tempdext}"
 		done
 	done
 
 	#Rename all *.gz to *.nii.gz and move to correct folder
-	for i in sub-${subj}_*.gz; do
-		mv $i "${i%.*}.nii.gz"
-	done
+	for i in sub-${subj}_*.gz; do mv $i "${i%.*}.nii.gz"; done
 	for files in *_dwi*; do mv $files dwi; done
-
-
-
-
 
 	#Handle duplicate files
 	#for file in *~*; do
@@ -209,66 +303,6 @@ for subj in $subjects; do  #change this to the folder you want it to be
 	#	mv $file ${file//.${dupext}/}
 	#	mv $dupfile ${dupfile//~*/~-duplicate.${dupext}}
 	#done
-
-
-	###Check func json for required fields
-	#Required fields for func: 'RepetitionTime','VolumeTiming' or 'SliceTiming', and 'TaskName'
-	#capture all jsons to test
-	cd ${niidir}/sub-${subj}/func #Go into the func folder
-	for funcjson in $(ls *.json*); do
-
-		#Repeition Time exist?
-		repeatexist=$(cat ${funcjson} | jq '.RepetitionTime')
-		if [[ ${repeatexist} == "null" ]]; then
-			echo "${funcjson} doesn't have RepetitionTime defined"
-		else
-			echo "${funcjson} has RepetitionTime defined"
-		fi
-
-		#VolumeTiming or SliceTiming exist?
-		#Constraint SliceTiming can't be great than TR
-		volexist=$(cat ${funcjson} | jq '.VolumeTiming')
-		sliceexist=$(cat ${funcjson} | jq '.SliceTiming')
-		if [[ ${volexist} == "null" && ${sliceexist} == "null" ]]; then
-			echo "${funcjson} doesn't have VolumeTiming or SliceTiming defined"
-		else
-			if [[ ${volexist} == "null" ]]; then
-				echo "${funcjson} has SliceTiming defined"
-				#Check SliceTiming is less than TR
-				sliceTR=$(cat ${funcjson} | jq '.SliceTiming[] | select(.>="$repeatexist")')
-				if [ -z ${sliceTR} ]; then
-					echo "All SliceTiming is less than TR" #The slice timing was corrected in the newer dcm2niix version called through command line
-				else
-					echo "SliceTiming error"
-				fi
-			else
-				echo "${funcjson} has VolumeTiming defined"
-			fi
-		fi
-
-		#Does TaskName exist?
-		taskexist=$(cat ${funcjson} | jq '.TaskName')
-		if [ "$taskexist" == "null" ]; then
-			jsonname="${funcjson%.*}"
-			taskfield=$(echo $jsonname | cut -d '_' -f2 | cut -d '-' -f2)
-			jq '. |= . + {"TaskName":"'${taskfield}'"}' ${funcjson} > tasknameadd.json
-			rm ${funcjson}
-			mv tasknameadd.json ${funcjson}
-			echo "TaskName was added to ${jsonname} and matches the tasklabel in the filename"
-		else
-			Taskquotevalue=$(jq '.TaskName' ${funcjson})
-			Taskvalue=$(echo $Taskquotevalue | cut -d '"' -f2)
-			jsonname="${funcjson%.*}"
-			taskfield=$(echo $jsonname | cut -d '_' -f2 | cut -d '-' -f2)
-			if [ $Taskvalue == $taskfield ]; then
-				echo "TaskName is present and matches the tasklabel in the filename"
-			else
-				echo "TaskName and tasklabel do not match"
-			fi
-		fi
-
-	done
-done
 
 echo -n "Do you want to compress the dicom files after conversion (y/n)? "
 read DOcompress
